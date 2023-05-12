6214d8ade255267b2188a203b174cad5c4d0ce93
ElDav1d <dvivoes@yahoo.es>
2023-05-12 18:15:23 +0200

    chire: install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-import "./App.css";
 import { CountdownList } from "./countdowns/CountdownList/CountdownList";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <QueryClientProvider client={queryClient}>
         <header>
           <h1>A list of countdowns</h1>
